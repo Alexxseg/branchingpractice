bbbdbd7d9f0b9310fd4b2f1832c17b31c39c76db
Alex Segura <alexsegura72@yahoo.com>
2022-06-06 16:10:52 -0400

    first backup of the project

--- src/test/java/ExistInMainBranch.java
+++ src/test/java/ExistInMainBranch.java
@@ -1,2 +1,6 @@
 public class ExistInMainBranch {
+    public static void main(String[] args) {
+        System.out.println("This class exist in ");
+        System.out.println("main copy of the project");
+    }
 }
